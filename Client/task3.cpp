825e9266995287d2703773ba7b4155712916434a
AbdullaevOlim <113762424+AbdullaevOlim@users.noreply.github.com>
2023-06-06 21:39:57 +0300

    task_abdullaev

---
+++ Client/task3.cpp
@@ -0,0 +1,67 @@
+#include "task3.h"
+#include "ui_task3.h"
+#include <functionsforclient.h>
+
+task3::task3(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::task3)
+{
+    ui->setupUi(this);
+}
+
+task3::~task3()
+{
+    delete ui;
+}
+
+void task3::setTaskNumber(int n, QString _log, int vari)
+{
+    task_number = n;
+    log = _log;
+    var = vari;
+    ui->label_3->clear();
+    ui->lineEdit_answer_3->clear();
+    show();
+}
+
+void task3::on_send_answer_but_clicked()
+{
+    QString result_from_user = ui->lineEdit_answer_3->text();
+    QString a = check_ans(task_number, QString::number(var), result_from_user, log);
+    qDebug()<< a;
+    if (a == "true"){
+        ui->label_3->setText("Гамильтон!");
+    }
+    else{
+        ui->label_3->setText("Неправильно!");
+    }
+}
+
+void task3::generateTask(int n)
+{
+    QString res = "";
+        QSet<QString> edges;
+        int length = Random::get(2, 9); // Вершины
+        int kolvo_reber = Random::get(2, 5) % ((length * (length - 1)) / 2) + 1; // количество ребер
+        qDebug() << length << ' ' << kolvo_reber;
+        while (edges.size() < kolvo_reber) {
+            int vertex1 = Random::get(1, 9) % length+1;
+            int vertex2 = Random::get(1, 9) % length+1;
+
+            if (vertex1 < vertex2) {
+                edges.insert('(' + QString::number(vertex1) + ',' + QString::number(vertex2) + ')');
+            }
+            else if (vertex2 < vertex1) {
+                edges.insert('(' + QString::number(vertex2) + ',' + QString::number(vertex1) + ')');
+            }
+        }
+
+        for (auto it = edges.begin(); it != edges.end(); it++) {
+            res.append(*it);
+            res.append(",");
+        }
+        res = res.mid(0, res.size() - 1);
+        res = QString::number(length) + res;
+        ui->prufer->setText(res);
+    }
+}
