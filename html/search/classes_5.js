e5602dbed4239922ed48be87ccc6866bd0265064
AbdullaevOlim <113762424+AbdullaevOlim@users.noreply.github.com>
2023-06-26 17:33:45 +0300

    Doxigen

---
+++ html/search/classes_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['task1_0',['task1',['../classtask1.html',1,'']]],
+  ['task2_1',['task2',['../classtask2.html',1,'']]],
+  ['task3_2',['task3',['../classtask3.html',1,'']]]
+];
